8c22d34d85d33513d06eb4e06c2df49bef4b4938
iamsadat <sadatullahkhan40@gmail.com>
2023-07-04 18:11:05 +0530

    changed font and navbar styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Navbar from "./Navbar";
+import Navbar from "./components/Navbar";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import About from "./pages/About";
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="nav">
-      <Link to="/" className="title">
+      <Link to="/home" className="title">
         Universal Farms
       </Link>
       <ul>
