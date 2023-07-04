d4e7c6b1f11ce14ff8858fcd0dfe7d6616c946d2
shoebm891 <shoebm891@gmail.com>
2023-07-05 01:40:53 +0530

    Added contact form

---
+++ src/components/FormInput.js
@@ -0,0 +1,12 @@
+
+
+const FormInput =(props)=>{
+    return (
+        <div className="formInput">
+           
+            <input placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+export default FormInput
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,19 @@
+import React from "react";
+import FormInput from "../components/FormInput";
+
 export default function Home() {
-  return (
-    <>
-      <h1>Contact page</h1>
-    </>
-  );
-}
+    return (
+      <div>
+        <h1>Contact Us</h1>
+        <div className="contact">
+          <form>
+            <FormInput placeholder="Full Name"/>
+            <FormInput placeholder="Phone Number"/>
+            <FormInput placeholder="Email"/>
+          </form>
+        </div>
+      </div>
+    );
+  }
+
+  
